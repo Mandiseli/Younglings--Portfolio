a07f2ca468dac906762411781998b682726c931b
Mandiseli <mandiseli@outlook.com>
2024-02-06 15:50:18 +0200

    fix an error on my home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
 import "./App.css";
-import {Router, Route} from 'react-router-dom';
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
-import About from './components/about/About';
+import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
-
+//import Services from "./components/services/Services";
 
 const App = () => {
     return (
         <>
         <Header/>
-        
+
         <main className="main">
-            <Router>
-                <Route index path='/home' element={<Home/>}/> 
-                <Route index path='/about' element={<About/>}/>
-                <Route index path='/skills' element={<Skills/>}/>
-            </Router>
+            <Home />
+            <About/>
+            <Skills/>
+            
+
         </main>
         </>
     )
 }
 
-export default App;
+export default App
--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,21 +3,21 @@ import React from "react";
 const Info = () => {
     return (
         <div className="about_info grid">
-            <div className="about box">
+            <div className="about_box">
                 <i class="bx bx-award about_icon"></i>
 
                 <h3 className="about_title">Experience</h3>
                 <span className="about_subtitle">1 year Experience</span>
             </div>
 
-            <div className="about box">
+            <div className="about_box">
             <i class="bx bx-briefcase-alt about_icon"></i>
 
                 <h3 className="about_title">completed</h3>
                 <span className="about_subtitle">10 + Projects</span>
             </div>
 
-            <div className="about box">
+            <div className="about_box">
             <i class="bx bx-support about_icon"></i>
 
                 <h3 className="about_title">Support</h3>
